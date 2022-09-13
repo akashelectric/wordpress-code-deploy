606e2eddc4eec8e7b4c8fbbcbd2c717cc010e716
akashelectric <106246282+akashelectric@users.noreply.github.com>
2022-09-13 15:29:43 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', 'Akashv183@' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
